963e3d65f45746ac098fde2de476ab9f201b96b1
judylime <judylime@users.noreply.github.com>
2024-11-26 07:53:15 +0000

    deploy: f806f561a0e0cf512af28147891f72afb5e7ccbf

---
+++ precache-manifest.21db3fa19eb6c06993f0461b38f6a1c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b6934b1958d6e20276f0fa6ac3413c",
+    "url": "/SVCcamp/index.html"
+  },
+  {
+    "revision": "1d67a33f04f8d165e64a",
+    "url": "/SVCcamp/static/css/2.45dff86c.chunk.css"
+  },
+  {
+    "revision": "6489e8cb4aafc99b40de",
+    "url": "/SVCcamp/static/css/main.c6e0171e.chunk.css"
+  },
+  {
+    "revision": "1d67a33f04f8d165e64a",
+    "url": "/SVCcamp/static/js/2.b6573cb2.chunk.js"
+  },
+  {
+    "revision": "6489e8cb4aafc99b40de",
+    "url": "/SVCcamp/static/js/main.72951569.chunk.js"
+  },
+  {
+    "revision": "fb804e587bc13bd77d27",
+    "url": "/SVCcamp/static/js/runtime~main.0d3aac37.js"
+  },
+  {
+    "revision": "5056ec3e3cc9ca42d2f4dee7929b0af5",
+    "url": "/SVCcamp/static/media/image.5056ec3e.png"
+  }
+]);
